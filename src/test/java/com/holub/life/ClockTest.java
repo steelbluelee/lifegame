c3f1ff5bc25cdc2342536682711ee95ba435e212
SeungKyu Lee <steelblue.lee@gmail.com>
2017-08-31 06:19:42 +0900

    changed ClockTest not to use PowerMock

--- src/test/java/com/holub/life/ClockTest.java
+++ src/test/java/com/holub/life/ClockTest.java
@@ -1,24 +1,16 @@
 package com.holub.life;
 
 import org.junit.*;
-import org.powermock.modules.junit4.PowerMockRunner;
-import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
 
-import com.holub.ui.MenuSite;
-
-@RunWith(PowerMockRunner.class)
 public class ClockTest {
     Clock clock = Clock.instance();
 
     @Test
-    @PrepareForTest(MenuSite.class)
     public void test_tick() {
 
         Clock.Listener mockClockLintenen1 = Mockito.mock(Clock.Listener.class);
         Clock.Listener mockClockLintenen2 = Mockito.mock(Clock.Listener.class);
-
 
         Clock clock = Clock.instance();
 
