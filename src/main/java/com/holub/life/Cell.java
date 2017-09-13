fa748fd8be6645621ec585a9121a99a7e5873c1a
SeungKyu Lee <steelblue.lee@gmail.com>
2017-09-13 16:23:19 +0900

    Cell.java done

--- src/main/java/com/holub/life/Cell.java
+++ src/main/java/com/holub/life/Cell.java
@@ -49,16 +49,110 @@ public interface Cell
     int widthInCells();
 
     // 자신과 내용상 동일한 객체를 새로 생성하여 반환한다.
-    Cell cleate();
+    Cell create();
 
     /* 이 셀의 변화로 인해 상태가 변화가 될 셀의 방향을 나타내는
      * Direction 객체를 반환한다.
      * @return 변화가 일어난 엣지(edge) 혹은 엣지들(edges)을 나타내는
      *     Direction 객체
      *  */
-    Direction isDistuptiveTo();
+    Direction isDisruptiveTo();
 
     // 셀과 모든 서브셀을 '죽은' 상태로 셋팅한다.
     void clear();
 
+    /* Cell.Memento 인터페이스는 향후 복구를 위해
+     * 셀과 모든 서브셀의 상태를 저장한다. */
+    interface Memento extends Storable
+    {
+        /* 메멘토를 생성할 때 셀이 살아 있음을 표시한다. */
+        void markAsAlive( Point location );
+
+        /* 메멘토로부터 셀을 복구시킬 때
+         * 셀이 살아 있음을 알려 준다. */
+        boolean isAlive( Point location );
+    }
+
+    /* 이 메소드는 내부적으로 메멘토로부터 셀의 상태를 저장하거나
+     * 복구하기 위해 사용된다.
+     * @return true 셀이 transfer에 의해 수정되었을 때 */
+    boolean transfer( Storable memento, Point upperLeftCorner, boolean doLoad );
+
+    /* transfer() 메소드의 'doLoad' 인자로 넘겨주는 값 */
+    public static boolean STORE = false;
+
+    /* 이 메소드는 최외곽 셀에서 사용한다.
+     * 최외곽 셀이 아닌 내부 셀에서는 사용하지 않기 때문에
+     * 유니버스에 있는 최외곽 셀만이 구현하면 된다.
+     * 최외곽 셀이 아닌 다른 셀들은 이 메소드가 호출되었을 때
+     * UnsupportedOperationException을 던져야 한다. */
+    Storable createMemento();
+
+    /* DUMMY 싱글톤은 항상 죽어 있는(그러므로 안정된) 셀을 표현한다.
+     * 이는 격자의 엣지(edge)로 사용되며 싱글톤이다.
+     * Dummy 클래스는 private 이어야 하지만, 아래에 선언되어 있는
+     * public DUMMY 필드를 통해 접근할 수 있다. 나는 이 클래스가
+     * private 이길 바리지만, JLS(열자 주: Java Language Specification)는
+     * 인터페이스가 private 멤버를 갖는 것을 허용하지 않는다. */
+
+    public static final Cell DUMMY = new Cell()
+    {
+        public boolean figureNextState( Cell n, Cell s, Cell e, Cell w, Cell ne, Cell nw, Cell se,
+                Cell sw )
+        {
+            return true;
+        }
+
+        public Cell edge( int r, int c )
+        {
+            return this;
+        }
+
+        public boolean isAlive()
+        {
+            return false;
+        }
+
+        public Cell create()
+        {
+            return this;
+        }
+
+        public Direction isDisruptiveTo()
+        {
+            return Direction.NONE;
+        }
+
+        public void clear()
+        {
+        }
+
+        public int widthInCells()
+        {
+            return 0;
+        }
+
+        public boolean transition()
+        {
+            return false;
+        }
+
+        public void userClicked( Point h, Rectangle s )
+        {
+        }
+
+        public void redraw( Graphics g, Rectangle here, boolean drawAll )
+        {
+        }
+
+        public boolean transfer( Storable m, Point ul, boolean load )
+        {
+            return false;
+        }
+
+        public Storable createMemento()
+        {
+            throw new UnsupportedOperationException( "Cannot create memento of dummy block" );
+        }
+    };
 }
